fb870c6fb852b1e2f60e35f49beaf83b23235d07
BlueAccords <myoldemailname@gmail.com>
2016-01-11 12:54:27 -0800

    add validation for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
 	validates :title, presence: true, length: {in: 3..100}
 	validates :body, presence: true
 	belongs_to :user
+	has_many :posts, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {in: 8..100}
   validates :email, presence: true, length: {in: 3..50}, uniqueness: true
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
