4bc25792a9146c7bbb125d66f93fd8b274628de2
BlueAccords <myoldemailname@gmail.com>
2016-01-11 12:51:59 -0800

    added validations for posts/users and scaffolded comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true, length: {in: 3..100}
+	validates :body, presence: true
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, length: {in: 3..20}, uniqueness: true
   validates :password, presence: true, length: {in: 8..100}
   validates :email, presence: true, length: {in: 3..50}, uniqueness: true
+  has_many :posts, dependent: :destroy
 end
